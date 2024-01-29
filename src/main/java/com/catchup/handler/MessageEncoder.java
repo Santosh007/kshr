6dd3ca7bc1698048596a594fafe1aaaf92621f44
Santosh <santoshreddy8341@live.com>
2024-01-29 18:36:26 +0530

    Webcom/kshr#4 Fix for log4j vulerability
    Signed-off-by: Santosh <santoshreddy8341@live.com>

--- src/main/java/com/catchup/handler/MessageEncoder.java
+++ src/main/java/com/catchup/handler/MessageEncoder.java
@@ -8,16 +8,18 @@ import javax.websocket.EncodeException;
 import javax.websocket.Encoder;
 import javax.websocket.EndpointConfig;
 
-import org.apache.log4j.Logger;
-
 import com.catchup.carrier.Message;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * @author Santosh
  *
  */
 public class MessageEncoder implements Encoder.Text<Message>{
-	final static Logger logger = Logger.getLogger(MessageEncoder.class);
+	//final static Logger logger = //Logger.getLogger(MessageEncoder.class);
+
+	final static Logger logger = LogManager.getLogger(MessageEncoder.class);
 
 	public void destroy() {
 		// TODO Auto-generated method stub
